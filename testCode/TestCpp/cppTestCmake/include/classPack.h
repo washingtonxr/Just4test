beee7312b1d622cb9388c5a21fa4a48435d0b426
Washington Ruan <washingtonxr@live.com>
2021-09-21 11:20:55 +0800

    testCode
    Add folder testCode.

---
+++ testCode/TestCpp/cppTestCmake/include/classPack.h
@@ -0,0 +1,11 @@
+#ifndef _CLASSPACK_H_
+#define _CLASSPACK_H_
+
+int testClass(void);
+int testClass2(void);
+int privateClass(void);
+int protectClass(void);
+int publicInheritance(void);
+
+#endif
+/* End of this file. */
